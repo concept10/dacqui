54abac4f4b9e3f8e2ad2e41b9a0599222d7afbe7
concept10 <tenthconcept@gmail.com>
2024-11-08 03:01:50 -0500

    Add TransportFactory class for creating transport instances

---
+++ transportFactory.js
@@ -0,0 +1,24 @@
+// transportFactory.js
+
+const TcpTransport = require('./transports/tcpTransport');
+// const UdpTransport = require('./transports/udpTransport');
+// const UnixSocketTransport = require('./transports/unixSocketTransport');
+
+class TransportFactory {
+  static createTransport(config) {
+    const { type } = config;
+
+    switch (type) {
+      case 'tcp':
+        return new TcpTransport(config.host, config.port);
+      case 'udp':
+        return new UdpTransport(config.host, config.port);
+      case 'unix':
+        return new UnixSocketTransport(config.path);
+      default:
+        throw new Error('Unknown transport type');
+    }
+  }
+}
+
+module.exports = TransportFactory;
