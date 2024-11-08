edfda7279c6a487c048e577dddb383c413f4bc11
concept10 <tenthconcept@gmail.com>
2024-11-08 11:59:40 -0500

    Refactor S7Transport class to use 'self' for context in promise callbacks

--- s7transport.js
+++ s7transport.js
@@ -25,16 +25,17 @@ class S7Transport extends EventEmitter {
    * @returns {Promise<void>} Resolves when connected.
    */
   connect() {
-    return new Promise((resolve, reject) => {
-      this.client.connect(this.port, this.host, () => {
+    var self = this;
+    return new Promise(function(resolve, reject) {
+      self.client.connect(self.port, self.host, function() {
         console.log('Connected to PLC');
-        this.emit('connected');
+        self.emit('connected');
         resolve();
       });
 
-      this.client.on('error', (err) => {
+      self.client.on('error', function(err) {
         console.error('Connection error:', err);
-        this.emit('error', err);
+        self.emit('error', err);
         reject(err);
       });
     });
@@ -45,22 +46,23 @@ class S7Transport extends EventEmitter {
    * @returns {Promise<Buffer>} Resolves with the connection response.
    */
   sendConnectionRequest() {
+    var self = this;
     const connectionRequest = Buffer.from([
       0x03, 0x00, 0x00, 0x16, 0x11, 0xE0, 0x00, 0x00, 0x00, 0x01, 0x00, 0xC1, 0x02, 0x01, 0x00, 0xC2, 0x02, 0x01, 0x02, 0xC0, 0x01, 0x09
     ]);
 
-    return new Promise((resolve, reject) => {
-      this.client.write(connectionRequest);
+    return new Promise(function(resolve, reject) {
+      self.client.write(connectionRequest);
 
-      this.client.once('data', (data) => {
+      self.client.once('data', function(data) {
         console.log('Connection response:', data);
-        this.emit('connectionResponse', data);
+        self.emit('connectionResponse', data);
         resolve(data);
       });
 
-      this.client.once('error', (err) => {
+      self.client.once('error', function(err) {
         console.error('Connection request error:', err);
-        this.emit('error', err);
+        self.emit('error', err);
         reject(err);
       });
     });
@@ -93,22 +95,23 @@ class S7Transport extends EventEmitter {
    * @returns {Promise<Buffer>} Resolves with the read data.
    */
   sendReadRequest(area, dbNumber, start, amount) {
+    var self = this;
     const readRequest = Buffer.from([
       0x03, 0x00, 0x00, 0x1F, 0x02, 0xF0, 0x80, 0x32, 0x01, 0x00, 0x00, 0x00, 0x01, 0x00, 0x0E, 0x00, 0x04, 0x01, 0x12, 0x0A, 0x10, 0x02, 0x00, 0x01, 0x00, 0x00, area, dbNumber, start, amount
     ]);
 
-    return new Promise((resolve, reject) => {
-      this.client.write(readRequest);
+    return new Promise(function(resolve, reject) {
+      self.client.write(readRequest);
 
-      this.client.once('data', (data) => {
+      self.client.once('data', function(data) {
         console.log('Read response:', data);
-        this.emit('readResponse', data);
+        self.emit('readResponse', data);
         resolve(data);
       });
 
-      this.client.once('error', (err) => {
+      self.client.once('error', function(err) {
         console.error('Read request error:', err);
-        this.emit('error', err);
+        self.emit('error', err);
         reject(err);
       });
     });
@@ -124,22 +127,23 @@ class S7Transport extends EventEmitter {
    * @returns {Promise<Buffer>} Resolves with the write response.
    */
   sendWriteRequest(area, dbNumber, start, amount, value) {
+    var self = this;
     const writeRequest = Buffer.from([
       0x03, 0x00, 0x00, 0x21, 0x02, 0xF0, 0x80, 0x32, 0x01, 0x00, 0x00, 0x00, 0x01, 0x00, 0x10, 0x00, 0x05, 0x01, 0x12, 0x0A, 0x10, 0x02, 0x00, 0x01, 0x00, 0x00, area, dbNumber, start, amount, value
     ]);
 
-    return new Promise((resolve, reject) => {
-      this.client.write(writeRequest);
+    return new Promise(function(resolve, reject) {
+      self.client.write(writeRequest);
 
-      this.client.once('data', (data) => {
+      self.client.once('data', function(data) {
         console.log('Write response:', data);
-        this.emit('writeResponse', data);
+        self.emit('writeResponse', data);
         resolve(data);
       });
 
-      this.client.once('error', (err) => {
+      self.client.once('error', function(err) {
         console.error('Write request error:', err);
-        this.emit('error', err);
+        self.emit('error', err);
         reject(err);
       });
     });
