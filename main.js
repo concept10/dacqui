81e91d15e77e4b387ede9621069ca5e9f6f4873e
Tracey <tenthconcept@gmail.com>
2024-11-08 02:41:32 -0500

    Update main.js
    add transports - pass to runtime and post!

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 // main.js
+
+const dacqui = require('./daquiRuntime');
+const transportConfig = {
+    type: 'tcp',
+    host: '127.0.0.1',
+    port: '1986',
+};
+
+const runtime = new daqui(transportConfig);
+
 (async () => {
   await runtime.init();
 
@@ -6,3 +16,6 @@
     runtime.acquireSendData();
 }, 5000); // 
 })();
+
+post();
+
