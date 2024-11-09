838b559bca896c57293d5ad86d18b79ffeb54e39
concept10 <tenthconcept@gmail.com>
2024-11-09 00:00:10 -0500

    Refactor events.js to use ES6 module syntax and enhance event handling with additional event types

--- events.js
+++ events.js
@@ -1,22 +1,48 @@
 // events.js
 
-const EventEmitter = require('events');
+import { EventEmitter } from 'events';
+
+const EVENTS = {
+    INITIALIZED: 'initialized',
+    DATA_SENT: 'dataSent',
+    NEW_LISTENER: 'newListener',
+    DATA_RECEIVED: 'dataReceived',
+    ERROR: 'error',
+    CONNECTION_ESTABLISHED: 'connectionEstablished',
+    CONNECTION_CLOSED: 'connectionClosed',
+};
 
 class RuntimeEvents extends EventEmitter {
     constructor() {
         super();
-        this.on('newListener', (event, listener) => {
-        console.log(`New listener: ${event}`);
+        this.on(EVENTS.NEW_LISTENER, (event, listener) => {
+            console.log(`New listener: ${event}`);
         });
+    }
+
+    initialize() {
+        this.emit(EVENTS.INITIALIZED);
+    }
+
+    sendData(data) {
+        this.emit(EVENTS.DATA_SENT, data);
+    }
+
+    receiveData(data) {
+        this.emit(EVENTS.DATA_RECEIVED, data);
+    }
+
+    handleError(error) {
+        this.emit(EVENTS.ERROR, error);
+    }
+
+    establishConnection() {
+        this.emit(EVENTS.CONNECTION_ESTABLISHED);
+    }
+
+    closeConnection() {
+        this.emit(EVENTS.CONNECTION_CLOSED);
     }
 }
 
-// Enumerate event names
-const EVENTS = {
-  INITIALIZED: 'initialized',
-  DATA_SENT: 'dataSent',
-  NEW_LISTENER: 'newListener',
-
-};
-
-module.exports = { RuntimeEvents, EVENTS };
+export { RuntimeEvents, EVENTS };
